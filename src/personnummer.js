6edc43ab822f68024d26c6da29d6183605eb16f3
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2019-11-11 09:51:17 +0100

    Set separator to default value if missing

--- src/personnummer.js
+++ src/personnummer.js
@@ -76,21 +76,17 @@ const getParts = (ssn) => {
     century = ('' + (baseYear - ((baseYear - year) % 100))).substr(0, 2);
   }
 
-  if (sep !== '-' && sep !== '+') {
-    if ((new Date).getFullYear() - parseInt(century + year, 10) < 100) {
-      sep = '-';
-    } else {
-      sep = '+';
-    }
-  } else {
-    // Set the right separator to match the full year.
-    // >= 100 should use + and < 100 should use -
-    const yearDiff = (new Date().getFullYear()) - parseInt(century + year, 10);
-    if (sep === '-' && yearDiff >= 100) {
-      sep = '+';
-    } else if (sep === '+' && yearDiff < 100) {
-      sep = '-';
-    }
+  if (!sep) {
+    sep = '-';
+  }
+
+  // Set the right separator to match the full year.
+  // >= 100 should use + and < 100 should use -
+  const yearDiff = (new Date().getFullYear()) - parseInt(century + year, 10);
+  if (sep === '-' && yearDiff >= 100) {
+    sep = '+';
+  } else if (sep === '+' && yearDiff < 100) {
+    sep = '-';
   }
 
   return {
