be85b36fccd0f4fc75dd76cf157783f9442e6c80
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-08-19 15:55:43 +0200

    Fix unnecessary escape character

--- src/index.ts
+++ src/index.ts
@@ -181,7 +181,7 @@ class Personnummer {
    */
   // eslint-disable-next-line
   private parse(ssn: string, options?: OptionsType) {
-    const reg = /^(\d{2}){0,1}(\d{2})(\d{2})(\d{2})([\+\-\s]?)((?!000)\d{3})(\d)$/;
+    const reg = /^(\d{2}){0,1}(\d{2})(\d{2})(\d{2})([+\-\s]?)((?!000)\d{3})(\d)$/;
     const match = reg.exec(ssn);
 
     if (!match) {
